d1e7acecf03611e782189b0293ee5aa02caf80a3
Benjamin Dehlin <ben.dehlin@gmail.com>
2020-04-08 02:25:24 -0600

    useInput hook version 1

---
+++ src/hooks/useInput.js
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+const useInput = (initialState) => {
+  const [values, setValues] = useState(initialState)
+  return [
+    values,
+    ({ target }) => {
+      setValues({
+        ...values,
+        [target.name]: target.value,
+      })
+    },
+    () => {
+      for(let key in values){
+        setValues({[key]: ''})
+      }
+    }
+  ]
+}
+
+export default useInput
